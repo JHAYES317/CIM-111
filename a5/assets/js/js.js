2f23a745ecaa673458d17fb6f08f3da47696a076
Josh Hayes <jah409@miami.edu>
2019-11-25 15:29:46 -0800

    Update js.js

--- a5/assets/js/js.js
+++ a5/assets/js/js.js
@@ -1,1 +1,26 @@
-var likeGuitar = prompt("Do you like rock guitar? Input yes or no.");
+$(".services").hide();
+$(".Lessons").hide();
+$(".rates").hide();
+$(".text").hide();
+$(".Sessions").hide();
+$(".pricing").hide();
+$(".pricing").hide();
+$(".Accompaniment").hide();
+$(".fees").hide();
+$(".pricing").hide();
+$(".Address").hide();
+var likeGuitar = prompt("Do you like music? Input yes or no.");
+if(likeGuitar = "yes"){
+	$(".services").show();
+$(".Lessons").show();
+$(".rates").show();
+$(".text").show();
+$(".Sessions").show();
+$(".pricing").show();
+$(".pricing").show();
+$(".Accompaniment").show();
+$(".fees").show();
+$(".pricing").show();
+$(".Address").show();
+	
+}
